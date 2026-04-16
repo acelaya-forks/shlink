46d58bdd006f8460002e20691dee9717a991e840
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-04-16 17:46:50 +0200

    Fix long URL edition via CLI

---
+++ module/CLI/test-cli/Command/EditShortUrlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ShlinkioCliTest\Shlink\CLI\Command;
+
+use PHPUnit\Framework\Attributes\Test;
+use Shlinkio\Shlink\CLI\Command\ShortUrl\EditShortUrlCommand;
+use Shlinkio\Shlink\CLI\Command\ShortUrl\ResolveUrlCommand;
+use Shlinkio\Shlink\TestUtils\CliTest\CliTestCase;
+use Symfony\Component\Console\Command\Command;
+
+class EditShortUrlTest extends CliTestCase
+{
+    #[Test]
+    public function longUrlCanBeEdited(): void
+    {
+        [$originalOutput] = $this->exec([ResolveUrlCommand::NAME, 'abc123']);
+        self::assertStringContainsString('Long URL: https://shlink.io', $originalOutput);
+
+        [, $exitCode] = $this->exec([EditShortUrlCommand::NAME, 'abc123', '--long-url', 'https://example.com']);
+        self::assertEquals(Command::SUCCESS, $exitCode);
+
+        [$newOutput] = $this->exec([ResolveUrlCommand::NAME, 'abc123']);
+        self::assertStringContainsString('Long URL: https://example.com', $newOutput);
+    }
+}
